81e5b29348bfb3f0e9f67242999e7f972b22620f
Damian Bartosiak <bartosiak.contact@gmail.com>
2024-01-09 14:40:53 +0100

    Add password encryption logic

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -6,4 +7,26 @@ const UserSchema = new Schema({
     password: { type: String, required: true },
 });
 
+UserSchema.pre("save", function (next) {
+    const user = this;
+
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            res.send(err);
+        }
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) {
+                res.send(err);
+            }
+
+            user.password = hash;
+            next();
+        });
+    });
+});
 module.exports = mongoose.model("User", UserSchema);
